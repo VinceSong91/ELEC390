5fb554ba34f375d189289b2deb3fba624d8a7684
VinceSong91 <21VS51@queensu.ca>
2025-03-24 18:36:23 -0400

    Update greyscale_turn.py

--- greyscale_turn.py
+++ greyscale_turn.py
@@ -43,11 +43,12 @@ def wait_for_user_input():
 
         if user_input == "1":
             print("Turning left.")
-            px.set_dir_servo_angle(-76)  # Adjust the angle for left turn
-            px.forward(5)  # Move forward slowly while turning
             while True:
+                px.set_dir_servo_angle(-76)  # Adjust the angle for left turn
+                px.forward(5)  # Move forward slowly while turning
                 sensor_values = px.get_grayscale_data()
                 left_sensor = sensor_values[0]
+
                 if left_sensor > 200:  # Left sensor detects the line
                     print("Left line detected! Stopping turn.")
                     px.set_dir_servo_angle(-13)  # Straighten wheels
@@ -56,9 +57,9 @@ def wait_for_user_input():
 
         elif user_input == "2":
             print("Turning right.")
-            px.set_dir_servo_angle(50)  # Adjust the angle for right turn
-            px.forward(5)  # Move forward slowly while turning
             while True:
+                px.set_dir_servo_angle(50)  # Adjust the angle for right turn
+                px.forward(5)  # Move forward slowly while turning
                 sensor_values = px.get_grayscale_data()
                 right_sensor = sensor_values[2]
                 if right_sensor > 200:  # Right sensor detects the line
