c4ce6bbcff664b983ba57d7e961dde870757265d
VinceSong91 <21VS51@queensu.ca>
2025-03-24 19:24:40 -0400

    Update greyscale_turn.py

--- greyscale_turn.py
+++ greyscale_turn.py
@@ -45,7 +45,7 @@ def wait_for_user_input():
             px.turn_signal_left_on()
 
             print("Turning left.")
-            px.set_dir_servo_angle(-55)  # Adjust the angle for left turn
+            px.set_dir_servo_angle(-40)  # Adjust the angle for left turn
             px.forward(5)  # Move forward slowly while turning
             time.sleep(1)
             while True:
@@ -62,7 +62,7 @@ def wait_for_user_input():
             px.turn_signal_right_on()
 
             print("Turning right.")
-            px.set_dir_servo_angle(30)  # Adjust the angle for right turn
+            px.set_dir_servo_angle(25)  # Adjust the angle for right turn
             px.forward(5)  # Move forward slowly while turning
             time.sleep(1)
             while True:
