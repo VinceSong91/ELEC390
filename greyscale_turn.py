92aca2f71dec3e3fa626a76e321957cdc18fb5cc
VinceSong91 <21VS51@queensu.ca>
2025-03-24 17:32:00 -0400

    Update greyscale_turn.py

--- greyscale_turn.py
+++ greyscale_turn.py
@@ -44,13 +44,13 @@ def wait_for_user_input():
         if user_input == "1":
             print("Turning left.")
             px.set_dir_servo_angle(-76)  # Adjust the angle for left turn
-            time.sleep(5)  # Wait for servo to adjust (change if necessary)
+            time.sleep(500)  # Wait for servo to adjust (change if necessary)
             px.forward(5)  # Move forward slowly to complete turn
             break
         elif user_input == "2":
             print("Turning right.")
             px.set_dir_servo_angle(50)  # Adjust the angle for right turn
-            time.sleep(5)  # Wait for servo to adjust (change if necessary)
+            time.sleep(500)  # Wait for servo to adjust (change if necessary)
             px.forward(5)  # Move forward slowly to complete turn
             break
         elif user_input == "3":
