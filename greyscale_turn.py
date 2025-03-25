e60deef09b5f023bb8804ebb5db0fd56e4b42e30
VinceSong91 <21VS51@queensu.ca>
2025-03-25 14:44:38 -0400

    retry

--- greyscale_turn.py
+++ greyscale_turn.py
@@ -75,7 +75,7 @@ def right_turn():
     time.sleep(1)
     px.forward(10)
     px.set_dir_servo_angle(20)  # Right turn angle
-    music.music_play('~/ELEC390/tokyodrift-368bnuq4-38107.mp3')
+    music.music_play('~/ELEC390/tokyodrift.mp3')
     time.sleep(3.5)  # Turn for 1 second
     px.turn_signal_right_off()
     px.set_dir_servo_angle(NEUTRAL_ANGLE)
